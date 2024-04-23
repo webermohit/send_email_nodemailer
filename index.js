76db41f5ce53d05377dab1a8698f59103f9d6a1c
Mohit Pandey <67647538+webermohit@users.noreply.github.com>
2024-04-23 17:16:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const nodemailer = require("nodemailer");
+
+const server = http.createServer((request, response) => {
+    const auth = nodemailer.createTransport({
+        service: "gmail",
+        secure : true,
+        port : 465,
+        auth: {
+            user: "youremail@gmail.com",
+            pass: "your_password"
+
+        }
+    });
+
+    const receiver = {
+        from : "youremail@gmail.com",
+        to : "youremail@gmail.com",
+        subject : "Node Js Mail Testing!",
+        text : "Hello this is a text mail!"
+    };
+
+    auth.sendMail(receiver, (error, emailResponse) => {
+        if(error)
+        throw error;
+        console.log("success!");
+        response.end();
+    });
+    
+});
+
+server.listen(8080);
